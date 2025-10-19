68633444acdb382cf7cbb0bc9a8e25f2dc8f8dbb
ENZO F <enzoenzof2024@gmail.com>
2025-10-19 12:44:11 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBoTCgO7PoilzV9_IOoIfXVH-DuMED09-Y",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-9970817292-3ce01.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-9970817292-3ce01",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-9970817292-3ce01.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "469820488385",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:469820488385:web:e25afc5ec8df068edff240",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-W6XDK39F1K",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB8LV8AB2AyvU_LWk7Cy9xHtIt3xDP_WUY",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "viandmo-whitegreen.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "viandmo-whitegreen",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "viandmo-whitegreen.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "69187711611",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:69187711611:web:74c80cfc777a90405068de",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 function initializeFirebase() {
