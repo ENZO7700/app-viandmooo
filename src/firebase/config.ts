916a42a1f20b6f277b89c02401c743990e2f1c59
ENZO F <enzoenzof2024@gmail.com>
2025-10-20 11:24:57 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,29 +1,8 @@
-
-// @ts-nocheck
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB8LV8AB2AyvU_LWk7Cy9xHtIt3xDP_WUY",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "viandmo-whitegreen.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "viandmo-whitegreen",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "viandmo-whitegreen.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "69187711611",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:69187711611:web:74c80cfc777a90405068de",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  "projectId": "studio-9970817292-3ce01",
+  "appId": "1:469820488385:web:e25afc5ec8df068edff240",
+  "apiKey": "AIzaSyBoTCgO7PoilzV9_IOoIfXVH-DuMED09-Y",
+  "authDomain": "studio-9970817292-3ce01.firebaseapp.com",
+  "measurementId": "G-W6XDK39F1K",
+  "messagingSenderId": "469820488385"
 };
-
-function initializeFirebase() {
-  if (!getApps().length) {
-    return initializeApp(firebaseConfig);
-  } else {
-    return getApp();
-  }
-}
-
-const app = initializeFirebase();
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-
-export { app, auth, firestore, initializeFirebase };
