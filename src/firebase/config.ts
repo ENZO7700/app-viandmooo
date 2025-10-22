13e490b688ca03738193b1294eae4afc9c4d8b5e
ENZO F <enzoenzof2024@gmail.com>
2025-10-22 10:03:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9970817292-3ce01",
+  "appId": "1:469820488385:web:e25afc5ec8df068edff240",
+  "apiKey": "AIzaSyBoTCgO7PoilzV9_IOoIfXVH-DuMED09-Y",
+  "authDomain": "studio-9970817292-3ce01.firebaseapp.com",
+  "measurementId": "G-W6XDK39F1K",
+  "messagingSenderId": "469820488385"
+};
